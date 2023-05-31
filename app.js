e0ae4be974b142b98fab32e3dc852ecac265413c
Abdugafor Desktop <gafarovabdugafur@gmail.com>
2023-05-31 22:21:39 +0500

    fdasf

--- app.js
+++ app.js
@@ -114,20 +114,15 @@ particlesJS("particles-js", {
   
   /* ---- stats.js config ---- */
   
-  var count_particles, stats, update;
-  stats = new Stats;
-  stats.setMode(0);
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.left = '0px';
-  stats.domElement.style.top = '0px';
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector('.js-count-particles');
-  update = function() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);
+  new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor : ['#00ffff', '#02030c', '#02030c', '#02030c', '#02030c'],
+});
+
+const btn = document.querySelector('#btn')
+
+btn.addEventListener('click', () => {
+  fullpage_api.moveTo('slide2', 1);
+})
